e8257cf315a6f8d7fe6b42b5f116bae967da46da
Lucian Boatright <lucian.boatright@gmail.com>
2020-05-05 17:15:17 +0100

    showing two ways to render/compile react components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <h1>Hi I am the react app</h1>
-      </div>
-    );
+    // return (
+      // <div className="App">
+      //   <h1>Hi I am the react app</h1>
+      // </div>
+    // );
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+
   }
 }
 
