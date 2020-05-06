823c43492c273987aa3d52f374001ebf75438519
Lucian Boatright <lucian.boatright@gmail.com>
2020-05-06 12:30:51 +0100

    using state to add names and age

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: "Freddie", age: 30 },
+      { name: "Jason", age: 15 },
+      { name: "Micheal", age: 55 }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi I am the react app</h1>
         <p>This is under teh main</p>
-        <Person name="Nic" age= '28' />
-        <Person name="Celia" age= '70'  />
+        <button>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <Person name="James" age= '48' > Added content child: hobbie </Person>
         <Person name="Jeremy" age= '15'  />
       </div>
