428b5f79654dac8ccc22a8f8c617eabefb65a0b2
Lucian Boatright <lucian.boatright@gmail.com>
2020-05-21 16:30:24 +0100

    added conditional render component dependant on switch

--- src/App.js
+++ src/App.js
@@ -51,6 +51,26 @@ class App extends Component {
       cursor: 'pointer'
     }; // values need to be in quote marks as it is in js
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+      <div> 
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Change on line click')}
+          changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
+      </div> 
+      )
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
@@ -59,23 +79,7 @@ class App extends Component {
         style={style}
         onClick={this.togglePersonsHandeler}>Switch Name</button>
          {/* use ternary statemnt, if state.shoepwersons ?(ture) do __ :(else) null // put componets in div which can be managed   */}
-        { 
-        this.state.showPersons ? 
-          <div > 
-          <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age} />
-          <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Change on line click')}
-            changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
-          <Person 
-            name={this.state.persons[2].name} 
-            age={this.state.persons[2].age} />
-        </div> : null 
-        }
- 
+        {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
